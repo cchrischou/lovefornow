aa217ea6c510a8a2028bca57eb23fa154b78df4d
cchrischou <chouc513@newschool.edu>
2025-02-18 18:15:33 -0500

    fixedresponses

--- script12.js
+++ script12.js
@@ -1,11 +1,13 @@
 document.getElementById("clickMeButton").addEventListener("click", function() {
     const chatBox = document.getElementById("chatBox");
-    
-    // Create a new response message
+
     const responseMessage = document.createElement("div");
     responseMessage.classList.add("response");
     responseMessage.innerHTML = "<p>I cried to my mom because she kept making me do holiday things over winter break with her boyfriend and i didn’t want to 🤩</p>";
 
-    // Add the new message to the chat box
     chatBox.appendChild(responseMessage);
+
+    document.getElementById("clickMeButton").disabled = true;
+    
+    document.getElementById("clickMeButton").removeEventListener("click", arguments.callee);
 });
